4298b472125c7f7cf4189053db737160b0ee5fa0
namanyash <naman_yash@yahoo.in>
2022-08-21 19:47:03 +1000

    Prac 4

---
+++ server.js
@@ -0,0 +1,85 @@
+var express = require("express")
+
+var app = express()
+var cors = require('cors')
+
+let projectsCollection; 
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+
+const MongoClient = require('mongodb').MongoClient
+
+const uri = 'mongodb+srv://namanyash:admin@cluster0.yjgq3rt.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(uri, {useNewUrlParser: true})
+
+const createColllection = (collectionName) => {
+    client.connect((err,db) => {
+        projectsCollection = client.db().collection(collectionName);
+        if(!err) {
+            console.log('MongoDB Connected')
+        }
+        else {
+            console.log("DB Error: ", err);
+            process.exit(1);
+        }
+    })
+}
+
+const insertProjects = (project, callback) => {
+    projectsCollection.insert(project,callback);
+}
+
+const getProjects = (callback) => {
+    projectsCollection.find({}).toArray(callback);
+}
+
+const cardList = [
+    {
+        title: "Kitten 2",
+        image: "images/kitten-2.jpg",
+        link: "About Kitten 2",
+        desciption: "Demo desciption about kitten 2"
+    },
+    {
+        title: "Kitten 3",
+        image: "images/kitten-3.jpg",
+        link: "About Kitten 3",
+        desciption: "Demo desciption about kitten 3"
+    }
+]
+
+
+app.post('/api/projects',(req,res) => {
+    console.log("New Project added", req.body)
+    var newProject = req.body;
+    insertProjects(newProject,(err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Project Successfully added", data: result})
+        }
+    })
+})
+
+app.get('/api/projects',(req,res) => {
+    getProjects((err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Success", data: result})
+        }
+    })
+})
+
+
+var port = process.env.port || 3001;
+
+app.listen(port,()=>{
+    console.log("App listening to: "+port)
+    createColllection('Pets')
+})
